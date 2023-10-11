c951f9adc2027f8a8ac0f990ef6ebb70e8e7e7b6
jacekp <jacekpasiecki@icloud.com>
2023-10-11 23:37:38 +0200

    fit method for y with one feature/optimizator is now enum

--- src/LinearRegression/LinearRegression.hpp
+++ src/LinearRegression/LinearRegression.hpp
@@ -10,6 +10,7 @@
 #include <stdexcept>
 #include <sstream>
 #include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <iostream>
 
@@ -32,15 +33,52 @@ template <FloatingPoint datatype>
 class LinearRegression {
 
 public:
+    enum optimList{GRADIENTDESCENT, SGD, ADAM};
     explicit LinearRegression(float learningRate=0.01, unsigned int iterations=1000, double epsilon=1E-8):
-    learningRate(learningRate), iterations(iterations), epsilon(epsilon)
+    learningRate(learningRate), iterations(iterations), epsilon(epsilon), optimizator(optimList::GRADIENTDESCENT),\
+    adamParams(adamSettings{0.999,0.9,0})
     {};
 
     ~LinearRegression()=default;
 
     [[maybe_unused]] void fit(ublas::matrix<datatype> X, ublas::matrix<datatype> y)
     {
+        if(X.size1() != y.size1())
+            throw std::runtime_error("ERROR");
 
+        const auto num_samples = X.size1();
+        const auto num_features = X.size2();
+
+        coefficients_.resize(1, num_features, false);
+        weights.resize(num_features, 0.0);
+        bias = 0.0;
+
+        if(optimizator== optimList::GRADIENTDESCENT)
+        {
+            datatype loss = 0.0;
+            datatype error;
+            datatype prediction;
+            for (unsigned int iteration = 0; iteration < iterations; ++iteration) {
+                ublas::vector<datatype> gradient(num_features, 0.0);
+                for (size_t sample = 0; sample < num_samples; ++sample) {
+                    ublas::matrix_row<ublas::matrix<datatype>> x_sample(X, sample);
+                    prediction = ublas::inner_prod(x_sample, weights) + bias;
+                    error = prediction - y(sample, 0);
+                    loss += error * error;
+                    gradient += 2 * error * x_sample;
+                }
+                loss /= num_samples;
+                gradient /= num_samples;
+
+                weights -= learningRate * gradient;
+                bias -= learningRate * loss;
+
+                if (loss < epsilon) {
+                    break;
+                }
+            }
+            std::cout << error;
+        }
     }
 
     [[maybe_unused]] datatype getR2Score()
@@ -65,7 +103,7 @@ public:
     }
 
 private:
-    std::string optimizator="GradientDescent";
+    optimList optimizator;
     float learningRate;
     unsigned int iterations;
     double epsilon;
@@ -77,7 +115,7 @@ private:
     datatype bias=0.0;
     ublas::vector<datatype> weights;
 
-    [[maybe_unused]] adamSettings adamParams{0.9, 0.999, 0};
+    [[maybe_unused]] adamSettings adamParams;
 };
 
 #endif //MODERNML_LINEARREGRESSION_H
--- src/LinearRegression/main.cpp
+++ src/LinearRegression/main.cpp
@@ -8,11 +8,13 @@ int main()
     ublas::matrix<double> X(5, 2);
     ublas::matrix<double> y(5, 1);
 
-    for (size_t i = 0; i < X.size1(); ++i) {
-        for (size_t j = 0; j < X.size2(); ++j) {
+    for (size_t i = 0; i < X.size1(); ++i)
+    {
+        for (size_t j = 0; j < X.size2(); ++j)
+        {
             X(i, j) = static_cast<double>(i * X.size2() + j + 1);
         }
-        y(i, 0) = static_cast<double>(2 * i + 1);
+        y(i, 0) = static_cast<double>(i);
     }
 
     LinearRegression<double> model;
